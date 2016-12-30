438c41db9cc8677349e02f2b5013c913eff72757
Paul Stupka <psk@sitecore.net>
2016-12-30 18:49:32 +0200

    Initial fix

---
+++ src/Sitecore.Support.141098/ContentSearch/Client/Commands/RebuildAll.cs
@@ -0,0 +1,125 @@
+﻿namespace Sitecore.Support.ContentSearch.Client.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Linq;
+    using System.Threading;
+    using Sitecore.Abstractions;
+    using Sitecore.ContentSearch;
+    using Sitecore.ContentSearch.Commands;
+    using Sitecore.ContentSearch.Maintenance;
+    using Sitecore.Diagnostics;
+    using Sitecore.Jobs;
+    using Sitecore.Shell.Applications.Dialogs.ProgressBoxes;
+    using Sitecore.Shell.Framework.Commands;
+    using Sitecore.Web.UI.Sheer;
+
+    /// <summary>
+    /// Represents the indexing:rebuildall command.
+    /// </summary>
+    [Serializable]
+    public class RebuildAll : Command, IContentSearchCommand
+    {
+        /// <summary>
+        /// The translate.
+        /// </summary>
+        [NonSerialized]
+        private readonly ITranslate translate;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RebuildAll"/> class.
+        /// </summary>
+        public RebuildAll()
+        {
+            this.translate = ContentSearchManager.Locator.GetInstance<ITranslate>();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RebuildAll"/> class.
+        /// </summary>
+        /// <param name="translate">
+        /// The translate.
+        /// </param>
+        internal RebuildAll(ITranslate translate)
+        {
+            this.translate = translate;
+        }
+
+        #region Public methods
+
+        /// <summary>
+        /// Executes the command in the specified context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public override void Execute([NotNull] CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            Context.ClientPage.Start(this, "Run", new NameValueCollection());
+        }
+
+        /// <summary>
+        /// Runs the command in ProgressBox.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        protected void Run(ClientPipelineArgs args)
+        {
+            var jobName = string.Format("{0}", this.translate.Text(Sitecore.ContentSearch.Localization.Texts.RebuildingAllIndexes));
+            var headerText = this.translate.Text(Sitecore.ContentSearch.Localization.Texts.RebuildingAllIndexes);
+            ProgressBox.ExecuteSync(jobName, headerText, "People/16x16/hammer.png", this.RebuildAllIndexes, this.RebuildDone);
+        }
+
+        /// <summary>
+        /// The rebuild all indexes.
+        /// </summary>
+        /// <param name="args">
+        /// The Client Pipeline Args.
+        /// </param>
+        private void RebuildAllIndexes(ClientPipelineArgs args)
+        {
+            List<Job> jobs = IndexCustodian.RebuildAll(new[] { IndexGroup.Experience }).ToList();
+
+            while (jobs.Any(j => !j.IsDone))
+            {
+                Thread.Sleep(500);
+            }
+
+            int failed = jobs.Count(j => j.Status.Failed);
+
+            if (failed > 0)
+            {
+                args.Parameters["failed"] = "1";
+                args.Parameters["failedNumber"] = failed.ToString();
+                args.Parameters["totalNumber"] = jobs.Count.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Invoked when rebuild is done.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private void RebuildDone(ClientPipelineArgs args)
+        {
+            string message;
+            if (args.Parameters["failed"] == "1")
+            {
+                message = this.translate.Text(
+                    Sitecore.ContentSearch.Localization.Texts.RebuildingAllFailed,
+                    args.Parameters["failedNumber"],
+                    args.Parameters["totalNumber"]);
+            }
+            else
+            {
+                message = this.translate.Text(Sitecore.ContentSearch.Localization.Texts.AllIndexesRebuilt);
+            }
+
+            SheerResponse.Alert(message);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Sitecore.Support.141098/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.141098")]
+[assembly: AssemblyProduct("Sitecore.Support.141098")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("RebuildAll command causes SerializationException if custom (mongo, mssql, redis) session state provider is used.")]
+
